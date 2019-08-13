8507606d202016642aa6fa15f9f3e931dc961af5
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-08-13 17:27:18 -0500

    Add. Function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
@@ -55,3 +55,10 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Remove from self.__objects an existin object
+        """
+        k = type(obj).__name__ + '.' + obj.id
+        if (k in self.__objects):
+            self.__objects.pop(k, None)
