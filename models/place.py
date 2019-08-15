57401164c4c3ae4c77ee6406a843c1f796f83459
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-08-15 22:45:20 +0000

    Feature. Task 10

--- models/place.py
+++ models/place.py
@@ -6,6 +6,44 @@ from os import getenv
 from sqlalchemy.orm import relationship
 from models.review import Review
 
+"""
+Add an instance of SQLAlchemy Table called place_amenity for creating
+the relationship Many-To-Many between Place and Amenity:
+table name place_amenity
+metadata = Base.metadata
+2 columns:
+place_id, a string of 60 characters foreign key of places.id, primary
+key in the table and never null
+amenity_id, a string of 60 characters foreign key of amenities.id,
+primary key in the table and never null
+
+"""
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
+
+
+"""
+Update Place class:
+for DBStorage: class attribute amenities must represent a relationship with
+    the class Amenity but also as secondary to place_amenity with option
+    viewonly=False (place_amenity has been define previously)
+for FileStorage:
+Getter attribute amenities that returns the list of Amenity instances
+based on the attribute amenity_ids that contains all Amenity.id linked
+to the Place
+Setter attribute amenities that handles append method for adding an
+Amenity.id to the attribute amenity_ids. This method should accept only
+Amenity object, otherwise, do nothing.
+
+"""
+
 
 class Place(BaseModel, Base):
     """This is the class for Place
@@ -36,6 +74,9 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref='place', cascade='delete')
+        amenities = relationship(
+            'Amenity', secondary=place_amenity, viewonly=False)
+
     else:
         city_id = ""
         user_id = ""
@@ -48,6 +89,7 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
         @property
         def reviews(self):
             review_ls = []
@@ -56,3 +98,24 @@ class Place(BaseModel, Base):
                 if v.place_id == self.id:
                     review_ls.append(v)
             return review_ls
+
+        @property
+        def reviews(self):
+            amenities_ls = []
+            objects = storage.all(Amenity)
+            for k, v in objects.items():
+                if v.amenity_id == self.id:
+                    amenities_ls.append(v)
+            return amenities_ls
+
+"""
+Setter attribute amenities that handles append method for adding an
+Amenity.id to the attribute amenity_ids. This method should accept
+only Amenity object, otherwise, do nothing.
+"""
+
+        @property
+        def set_age(self, obj):
+            if isinstance(obj, Amenity):
+                amenity_id.append(obj.id)
+
