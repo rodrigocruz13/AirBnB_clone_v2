f9de3efb58a12dcdec73bf28c68de6dbf1be6c4d
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-08-15 22:44:44 +0000

    Feature. Task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel):
@@ -8,4 +9,13 @@ class Amenity(BaseModel):
     Attributes:
         name: input name
     """
+
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=True, primary_key=True)
+    place_amenities = relationship("Place", secondary=place_amenity)
+
+else:
     name = ""
+
