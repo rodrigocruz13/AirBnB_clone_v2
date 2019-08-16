58490d52f2f2afc8825a934ae6e09c54022be738
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-08-16 00:35:52 +0000

    AMenity file

--- models/amenity.py
+++ models/amenity.py
@@ -2,20 +2,21 @@
 """This is the amenity class"""
 
 from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
 
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=True, primary_key=True)
+        place_amenities = relationship("Place", secondary="place_amenity")
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=True, primary_key=True)
-    place_amenities = relationship("Place", secondary=place_amenity)
-
-else:
-    name = ""
-
+    else:
+        name = ""
