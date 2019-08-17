c2a6059f03b69ee699c9db67051c4a0a3917e26f
AndZapata <andzapata514@gmail.com>
2019-08-17 03:06:10 +0000

    [Fix][Feature] unittest

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -56,6 +56,8 @@ class TestCity(unittest.TestCase):
         self.assertEqual(type(self.city.name), str)
         self.assertEqual(type(self.city.state_id), str)
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     "not supported in this library version")
     def test_save_City(self):
         """test if the save works"""
         self.city.save()
