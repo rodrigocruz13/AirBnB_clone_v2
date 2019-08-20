aca94a0d284346152c18c261199af9e7256c9ff5
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-08-20 05:01:45 +0000

    Task 1. fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -17,13 +17,6 @@ def do_pack():
     full_date = now().strftime("%Y%m%d%H%M%S")
     full_name = "versions/web_static_{}.tgz".format(full_date)
 
-    """ c = create, v = verbose, z = zip, f = file """
-    local("tar -cvzf {} web_static".format(full_name))
-    return full_name
-
-    except:
-        return None
-
     with tarfile.open(full_name, "w:gz") as tar:
         tar.add("web_static", arcname=os.path.basename("web_static"))
     if os.path.exists(full_name):
