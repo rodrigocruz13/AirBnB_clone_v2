568544e2cbd506ca505204ed2c95b34be0869dbb
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-09-05 02:30:23 +0000

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+1. This script opens a Flask web application
+"""
+
+
+from flask import Flask
+from flask import escape
+from flask import render_template
+
+
+from models import storage, State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_states():
+    """ list the states of HBNB"""
+    state_dict = storage.all(State).values()
+    return render_template('8-cities_by_states.html', state_dict=state_dict)
+
+
+@app.teardown_appcontext
+def state_close(error):
+    """ close the connection"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
